12850a5e5f5c685d0c160e5538f8c718e16d38f1
nyobikou1 <nyobikou1@yahoo.co.jp>
2017-10-27 16:09:59 +0000

    fib関数の改善(メモ化)

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 'use strict';
 
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
+
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    //has メソッド 指定された要素があればtrueを返す
+    if (memo.has(n)) {  //memoの添字nに記憶していれば
+        return memo.get(n); //memoから答えを取り出す
     }
-    return fib(n - 1) + fib(n - 2);
+    const value = fib(n - 1) + fib(n - 2);  //新規計算
+    memo.set(n, value); //添字nのmapに答えを記憶させる
+    return value;
 }
 
-const length = 40;
+
+const length = 100;
 for (let i = 0; i <= length; i++) {
     console.log(fib(i));
 }
